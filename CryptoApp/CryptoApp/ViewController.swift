a2c792087362a4b35b5f6fadc226d9ae443ac080
manuel benites rojas <manuelbenites7@gmail.com>
2024-05-20 18:07:42 +0200

    initial xcode commit

---
+++ CryptoApp/CryptoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoApp
+//
+//  Created by manuel benites rojas on 20/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
